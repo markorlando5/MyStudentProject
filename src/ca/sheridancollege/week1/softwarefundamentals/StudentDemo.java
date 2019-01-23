1d60d58a63ac8e83396a30c507ae98e1e8fe5e5a
markorlando5 <mark.orlando@sheridancollege.ca>
2019-01-22 20:08:59 -0500

    added the date to the copyright section

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -8,6 +8,7 @@ package ca.sheridancollege.week1.softwarefundamentals;
 /**
  * A model class to model Student Objects
  * @author dancye, 2019
+ * @date January 21, 2019
  */
 public class StudentDemo 
 {
